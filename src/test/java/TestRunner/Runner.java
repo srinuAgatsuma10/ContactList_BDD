168e40532157fc6a990f2001f1b776cc776ea6af
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2026-02-19 14:44:08 +0530

    lazy run

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -15,7 +15,7 @@ import org.testng.annotations.*;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         monochrome = true,
-        tags = "@DataDriven"
+        tags = "@Smoke"
 )
 public class Runner extends AbstractTestNGCucumberTests {
         @Override
